f462ca12ba24201811602ac1b639083c910cdd50
Animeshcode007 <khareanimesh6@gmail.com>
2025-06-28 08:15:06 +0530

    feat: Configure global Axios base URL for production

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./contexts/AuthContext";
 import { SocketProvider } from "./contexts/SocketContext";
+import axios from 'axios';
+
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
